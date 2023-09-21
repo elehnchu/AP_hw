00cda4f31d663c6c5fa9be4e8753a84f81878547
elehnchu <123896882+elehnchu@users.noreply.github.com>
2023-09-21 10:47:43 -0400

    Delete hw2-team222 directory

--- hw2-team222/part1/src/iterative.h
+++
@@ -1,7 +0,0 @@
-#ifndef ITERATIVE_H_
-#define ITERATIVE_H_
-
-int gcd_iterative(int m, int n);
-
-
-#endif
--- hw2-team222/part1/src/recursive.h
+++
@@ -1,7 +0,0 @@
-#ifndef RECURSIVE_H_
-#define RECURSIVE_H_
-
-int gcd_recursive(int m, int n);
-
-
-#endif 
